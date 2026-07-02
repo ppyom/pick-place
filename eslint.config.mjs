4c8f763ed96a23b245867ec12b178a750c85f7bf
ppyom <ouob77@kakao.com>
2026-07-02 17:46:27 +0900

    🔧 ESLint, Prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,70 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import storybook from 'eslint-plugin-storybook';
+import unusedImports from 'eslint-plugin-unused-imports';
 
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-  ...storybook.configs["flat/recommended"]
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      /*
+       * Imports
+       */
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // React / Next / Third-party
+            ['^react$', '^react', '^next', '^@?\\w'],
+            // FSD Layers
+            ['^@/app'],
+            ['^@/providers'],
+            ['^@/widgets'],
+            ['^@/features'],
+            ['^@/entities'],
+            ['^@/shared'],
+            // Relative
+            ['^\\.'],
+            // Styles
+            ['^.+\\.css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      /*
+       * Unused Imports
+       */
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      /*
+       * JavaScript
+       */
+      'prefer-const': 'error',
+      'object-shorthand': ['error', 'always'],
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      'no-debugger': 'warn',
+    },
+  },
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  ...storybook.configs['flat/recommended'],
 ]);
-
-export default eslintConfig;
