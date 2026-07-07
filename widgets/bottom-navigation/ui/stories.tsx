75d7d1c6d7de13ad45084bedcd38ef9c648697e8
ppyom <ouob77@kakao.com>
2026-07-07 16:30:53 +0900

    📸 BottomNavigation 컴포넌트 스토리 작성

---
+++ widgets/bottom-navigation/ui/stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { BottomNavigation } from './bottom-navigation';
+
+const meta = {
+  title: 'Widgets/BottomNavigation',
+  component: BottomNavigation,
+  tags: [],
+} satisfies Meta<typeof BottomNavigation>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Home: Story = {
+  parameters: { nextjs: { navigation: { pathname: '/' } } },
+};
+
+export const Explore: Story = {
+  parameters: { nextjs: { navigation: { pathname: '/explore' } } },
+};
+
+export const My: Story = {
+  parameters: { nextjs: { navigation: { pathname: '/my' } } },
+};
