8532bb4aa0ed1abcf5e882aa4e94122b317a4238
ppyom <ouob77@kakao.com>
2026-07-20 22:27:54 +0900

    ✨ Input 컴포넌트 구현

---
+++ shared/ui/input/input.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, type InputHTMLAttributes } from 'react';
+
+import { Icon, type IconName } from '@/shared/ui/icon';
+import { cn } from '@/shared/utils/cn';
+
+import { inputIconVariants, type InputVariantProps, inputVariants } from './constants';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement>, Pick<InputVariantProps, 'status'> {
+  leftIcon?: IconName;
+}
+
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ className, leftIcon, status, ...props }, ref) => {
+    return (
+      <div className="relative">
+        {leftIcon && <Icon className={inputIconVariants({ status })} name={leftIcon} size="sm" />}
+        <input
+          ref={ref}
+          className={cn(inputVariants({ status, hasLeftIcon: !!leftIcon }), className)}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+
+Input.displayName = 'Input';
+
+export type { Props };
