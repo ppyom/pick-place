f5751837c81ff492d977cbf5c609c94f92fab299
ppyom <ouob77@kakao.com>
2026-08-13 23:15:15 +0900

    ✨ Login 페이지 컴포넌트 구현

---
+++ app/(routes)/login/page.tsx
@@ -0,0 +1,11 @@
+import { Suspense } from 'react';
+
+import { LoginContent } from './login-content';
+
+export default function LoginPage() {
+  return (
+    <Suspense>
+      <LoginContent />
+    </Suspense>
+  );
+}
