f7a6ff1ff59d1568f1af29ba676430af1a8b2ab9
ppyom <ouob77@kakao.com>
2026-08-13 15:16:09 +0900

    🩹 path 속성을 camelCase로 변경

--- features/oauth-button/ui/icons/kakao-logo.tsx
+++ features/oauth-button/ui/icons/kakao-logo.tsx
@@ -2,8 +2,8 @@ export function KakaoLogo() {
   return (
     <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M9.74359 0C4.59145 0 0 3.52032 0 7.86275C0 10.5626 1.77607 12.944 4.48034 14.3598L3.34245 18.4845C3.2416 18.8497 3.66211 19.14 3.98462 18.9285L8.97265 15.6619C9.39316 15.7019 9.82108 15.7255 9.74359 15.7255C15.9208 15.7255 20 12.2046 20 7.86275C20 3.52032 15.9208 0 9.74359 0Z"
         fill="black"
       />
