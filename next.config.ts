282935f4148fa9833d3213c06d626c7f6d5f5b8b
ppyom <ouob77@kakao.com>
2026-08-18 11:24:15 +0900

    🔧 next/image remotePatterns에 picsum.photos 임시 등록
    목데이터 이미지 도메인만 우선 등록. 실제 이미지 호스팅(S3/CDN 등) 정해지면 교체 예정.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // TODO: 실제 이미지 호스팅 도메인 정해지면 교체
+    remotePatterns: [{ protocol: 'https', hostname: 'picsum.photos' }],
+  },
 };
 
 export default nextConfig;
