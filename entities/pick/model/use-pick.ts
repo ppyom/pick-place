984c36cf74d405ead260a37fa4ff310e7ddfe760
ppyom <ouob77@kakao.com>
2026-08-25 22:49:37 +0900

    🐛 usePick에서 4xx에러는 재시도 하지 않게 수정

--- entities/pick/model/use-pick.ts
+++ entities/pick/model/use-pick.ts
@@ -2,11 +2,17 @@
 
 import { useQuery } from '@tanstack/react-query';
 
+import { ApiError } from '@/shared/api';
+
 import { getPick } from '../api/get-pick';
 
 export function usePick(id: string) {
   return useQuery({
     queryKey: ['pick', id],
     queryFn: () => getPick(id),
+    retry: (failureCount, error) => {
+      if (error instanceof ApiError && error.status < 500) return false;
+      return failureCount < 3;
+    },
   });
 }
