b4478244d2c7ef025d89fd6af87dbb9794279acb
ppyom <ouob77@kakao.com>
2026-08-25 16:18:50 +0900

    ✨ PlaceItem 컴포넌트 구현

--- entities/pick/index.ts
+++ entities/pick/index.ts
@@ -10,3 +10,4 @@ export { useSavedPicks } from './model/use-saved-picks';
 export { useSearchResults } from './model/use-search-results';
 export { BookmarkCount } from './ui/bookmark-count/bookmark-count';
 export { bookmarkCountVariants } from './ui/bookmark-count/constants';
+export { PlaceItem } from './ui/place-item/place-item';
